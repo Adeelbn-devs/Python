726917242d263d6e3ab72df1b997e57b84bc7c9f
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-08-04 23:13:51 +0530

    Data types

--- 01_Introduction/helloworld.py
+++ 01_Introduction/helloworld.py
@@ -1,13 +1,22 @@
 # Introduction
 
-print("Hello world!") #print hello world
+print("Hello world!")       #print hello world
 
-print(2+3) #addition(+)
-print(3-1) #subtraction(-)
-print(2*3) #multiplication(*)
-print(6/2) #division(/)
-print(3 ** 3) #exponential(**)
-print(3%2) #modulus(%)
-print(3 // 2) #floor division(//)
+print(2+3)                  #addition(+)
+print(3-1)                  #subtraction(-)
+print(2*3)                  #multiplication(*)
+print(6/2)                  #division(/)
+print(3 ** 3)               #exponential(**)
+print(3%2)                  #modulus(%)
+print(3 // 2)               #floor division(//)
 
+# Checking data types 
+
+print(type(10))                             #int
+print(type(3.15))                           #float
+print(type(1 + 3j))                         #complex
+print(type('Adeel'))                        #string
+print(type([1, 2, 3]))                      #list
+print(type({'name': 'Adeel', 'age': 21}))   #dictionary
+print(type({9.8, 3.14, 2.71}))              #set
 
