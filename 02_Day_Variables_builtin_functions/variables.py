f51c59663aacdb15ff44f15b6a56bae261a6a931
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-08-05 23:16:14 +0530

    Declaring variables

---
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -0,0 +1,16 @@
+## 💻 Step 3: Complete `variables.py` (Practice Code)
+
+# Topic 02: Practice Script
+
+# ==================== LEVEL 1 ====================
+# Declaring variables
+first_name = 'Adeel'
+last_name = 'B N'
+full_name = first_name + ' ' + last_name
+country = 'India'
+city = 'Mumbai'
+age = 21
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
