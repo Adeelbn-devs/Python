e7b76b375e2f7aad532d02e4494d7eb738d0e157
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-08-06 20:46:22 +0530

    Booleans
    Add overview of Python Booleans and Arithmetic.

---
+++ 03_Operators/operators.py
@@ -0,0 +1,8 @@
+# Topic 03: Operators & Booleans
+
+A clean overview of Python Booleans, Arithmetic
+
+---
+
+## 📌 1. Booleans
+Booleans represent truth values: `True` or `False` (First letter must be capitalized).
