354bd5e2e6be271eadba335f68ad52329f7ba03b
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-08-07 23:31:53 +0530

    Triangle Area Calculation

--- 03_Operators/Operators.py
+++ 03_Operators/Operators.py
@@ -4,3 +4,9 @@
 age = 19
 height = 5.9
 complex_num = 1 + 2j
+
+# 4. Triangle Area Calculation
+base = 20
+h_triangle = 10
+area_triangle = 0.5 * base * h_triangle
+print("Triangle Area:", area_triangle)
