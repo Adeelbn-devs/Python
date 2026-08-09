6bf3a6f57a90b34c9f211abc40e893a3b58beda5
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-08-09 23:11:12 +0530

    Concatenate

---
+++ 04_Strings/strings.py
@@ -0,0 +1,6 @@
+# Topic 04: Strings & String Methods Practice Script
+
+# 1. Concatenate 'Thirty', 'Days', 'Of', 'Python'
+word1, word2, word3, word4 = 'Thirty', 'Days', 'Of', 'Python'
+sentence1 = word1 + ' ' + word2 + ' ' + word3 + ' ' + word4
+print("1. Concatenated Sentence:", sentence1)
