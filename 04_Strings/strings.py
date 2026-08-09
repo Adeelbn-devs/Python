53a3814a3b15e67b74bb6715b4d683956bd57e74
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-08-09 23:12:23 +0530

    Concatenate Coding For All

--- 04_Strings/strings.py
+++ 04_Strings/strings.py
@@ -4,3 +4,7 @@
 word1, word2, word3, word4 = 'Thirty', 'Days', 'Of', 'Python'
 sentence1 = word1 + ' ' + word2 + ' ' + word3 + ' ' + word4
 print("1. Concatenated Sentence:", sentence1)
+
+# 2. Concatenate 'Coding', 'For', 'All'
+sentence2 = ' '.join(['Coding', 'For', 'All'])
+print("2. Sentence 2:", sentence2)
