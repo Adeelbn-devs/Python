95258955403a81576fd045946525906957c35436
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-08-13 22:47:13 +0530

    Create empty list, list with >5 items, find length

--- 05_Lists/lists.py
+++ 05_Lists/lists.py
@@ -2,3 +2,8 @@
 
 # ==================== LEVEL 1 ====================
 print("--- Level 1 ---")
+
+# 1, 2, 3. Create empty list, list with >5 items, find length
+empty_list = []
+numbers = [10, 20, 30, 40, 50, 60, 70]
+print("Length of numbers list:", len(numbers))
