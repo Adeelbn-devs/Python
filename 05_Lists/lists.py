e004eeaead44b554fafc58f759f39573225414ba
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-08-13 22:48:22 +0530

    Get first, middle, and last item

--- 05_Lists/lists.py
+++ 05_Lists/lists.py
@@ -7,3 +7,9 @@ print("--- Level 1 ---")
 empty_list = []
 numbers = [10, 20, 30, 40, 50, 60, 70]
 print("Length of numbers list:", len(numbers))
+
+# 4. Get first, middle, and last item
+first_item = numbers[0]
+middle_item = numbers[len(numbers) // 2]
+last_item = numbers[-1]
+print("First:", first_item, "| Middle:", middle_item, "| Last:", last_item)
