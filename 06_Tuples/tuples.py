669457548c02e4d139b2776e6a5f6b16739dab16
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-08-24 23:37:58 +0530

    Last 3 items

--- 06_Tuples/tuples.py
+++ 06_Tuples/tuples.py
@@ -25,3 +25,4 @@ print("Family Members:", family_members)
 
 # 6. Slice out the first three items and the last three items from food_staff_lt list
 print("First 3 items:", food_stuff_lt[:3])
+print("Last 3 items:", food_stuff_lt[-3:])
