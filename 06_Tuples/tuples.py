5add27d556543deee82d2ff1de208cc94138fcb6
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-08-24 23:38:57 +0530

    Check if an item exists in tuple

--- 06_Tuples/tuples.py
+++ 06_Tuples/tuples.py
@@ -30,3 +30,5 @@ print("Last 3 items:", food_stuff_lt[-3:])
 # 7. Delete the food_stuff_tp tuple completely
 del food_stuff_tp
 # print(food_stuff_tp) # This would throw a NameError because it is deleted
+
+# 8. Check if an item exists in tuple:
