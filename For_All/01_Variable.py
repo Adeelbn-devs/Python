a6c92d09e84a563c1d275cc648f13ed804e9f149
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-09-01 23:45:11 +0530

    This will print the value of the name variable

--- For_All/01_Variable.py
+++ For_All/01_Variable.py
@@ -1,4 +1,5 @@
 a = 3 # a is a variable that stores the value 3
 b = 2 # b is a variable that stores the value 2
 name = "Adil" # name is a variable that stores the value "Adil"
-print(a + b) 
+print(a + b) # This will print the sum of a and b, which is 5
+print(name) # This will print the value of the name variable
