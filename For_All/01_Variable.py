0efa72b9d26eab90e5f571f922897cbfc4f141bd
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-09-01 23:48:42 +0530

    one

--- For_All/01_Variable.py
+++ For_All/01_Variable.py
@@ -3,3 +3,6 @@ b = 2 # b is a variable that stores the value 2
 name = "Adil" # name is a variable that stores the value "Adil"
 print(a + b) # This will print the sum of a and b, which is 5
 print(name) # This will print the value of the name variable
+
+
+one = 2000
