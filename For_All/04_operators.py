e66d0bf3cf3fcdaf816d9ce8cea87b12526e458b
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-09-23 23:39:08 +0530

    Assignment Operators

--- For_All/04_operators.py
+++ For_All/04_operators.py
@@ -4,3 +4,11 @@ a = 7
 b = 4
 c = a + b 
 print(c) # This will print the sum of a and b, which is 11
+
+# Assignment Operators
+a = 4-2 # Assign 4-2 in a
+print(a)
+b = 6
+# b += 3 # Increment the value of b by 3 and then assign it to b
+b -= 3 # Decrement the value of b by 3 and then assign it to b
+print(b)
