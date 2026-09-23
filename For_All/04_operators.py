075e3c32294d712c667309fa8e296a6ea8efcdc8
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-09-23 23:49:44 +0530

    Logical Operators

--- For_All/04_operators.py
+++ For_All/04_operators.py
@@ -21,3 +21,13 @@ d = 5==5
 # d = 5<=5
 # d = 5!=5
 print(d)
+
+# Logical Operators
+
+e = True or False
+
+# Truth table of 'or' 
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
