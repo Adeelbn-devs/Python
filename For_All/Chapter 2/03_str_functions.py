31d9311161efa9d36cdd22b3d195d26e2a8e0c43
Adeelbn-devs <adeelbn.dev@gmail.com>
2026-09-26 23:58:57 +0530

    new Comments

--- For_All/Chapter 2/03_str_functions.py
+++ For_All/Chapter 2/03_str_functions.py
@@ -3,6 +3,6 @@ name = "Adeel"
 
 
 print(len(name)) # prints the length of the string
-print(name.endswith("el")) #
-print(name.startswith("Ad"))
-print(name.capitalize())
+print(name.endswith("el")) # checks if the string ends with "el"
+print(name.startswith("Ad")) # checks if the string starts with "Ad"
+print(name.capitalize()) # capitalizes the first letter of the string
